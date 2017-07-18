bbc0d3e58474659989eb144d913064436746c906
Franklin G. Mendoza <franklin.g.mendoza@gmail.com>
2017-07-18 00:32:44 -0400

    cache removes, notification method so-so

--- tobook/booking/models.py
+++ tobook/booking/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils.crypto import get_random_string
+from django.core.mail import send_mail
+from django.db.models import signals
 from object2book.models import (
     Statuses,
     Location,
     Object,
 )
+
 
 # Create your models here.
 
@@ -32,6 +35,18 @@ class Booking(models.Model):
     objectToBook = models.ForeignKey(Object)
     from_date = models.DateTimeField()
     to_date = models.DateTimeField()
+
+
+    def notify(sender, instance, created, **kwargs):
+        """Notify to user that a new booking has been added."""
+        if created:
+            subject = 'Booking created'
+            message = 'Booking %s for %s was added booked from %s to %s' % instance.pnr, instance.objectToBook, instance.from_date, instance.to_date
+            from_addr = 'no-reply@example.com'
+            recipient_list = (Person.email())
+            send_mail(subject, message, from_addr, recipient_list)
+
+        signals.post_save.connect(notify, sender=Booking)
 
     def __str__(self):
         return self.pnr
