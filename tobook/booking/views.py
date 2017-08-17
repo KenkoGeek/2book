929ac6ad4003d44dd4f87798eece2481288a3309
Franklin García <franklin.garcia@pawadominicana.com>
2017-08-17 11:01:55 -0400

    a simple view added for booking app

--- tobook/booking/views.py
+++ tobook/booking/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Booking, Person
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the booking index.")
